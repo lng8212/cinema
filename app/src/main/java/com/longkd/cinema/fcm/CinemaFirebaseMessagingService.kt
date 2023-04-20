53565fcf364ce964a041ae376dc1bf9ab28326ca
Long <long05012001@gmail.com>
2023-04-20 08:42:58 +0700

    add fcm notification

---
+++ app/src/main/java/com/longkd/cinema/fcm/CinemaFirebaseMessagingService.kt
@@ -0,0 +1,49 @@
+package com.longkd.cinema.fcm
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.longkd.cinema.R
+
+
+class CinemaFirebaseMessagingService : FirebaseMessagingService() {
+    companion object {
+        const val CHANNEL_ID = "HEADS_UP_NOTIFICATION"
+        const val CHANNEL_NAME = "HEADS_UP_NOTIFICATION"
+    }
+
+    override fun onNewToken(token: String) {
+        super.onNewToken(token)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate() {
+        super.onCreate()
+        val channel = NotificationChannel(
+            CHANNEL_ID,
+            CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_HIGH
+        )
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        val title = remoteMessage.notification!!.title
+        val text = remoteMessage.notification!!.body
+
+        val notification: NotificationCompat.Builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle(title)
+            .setContentText(text)
+            .setSmallIcon(R.drawable.ic_notification)
+            .setAutoCancel(true)
+
+        NotificationManagerCompat.from(this).notify(1, notification.build())
+        super.onMessageReceived(remoteMessage)
+    }
+}
